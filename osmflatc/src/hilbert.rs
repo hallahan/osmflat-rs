1a4af79fb1f3140c0ad3ed0db4f43517db3cde60
Nick Hallahan <nick@theoutpost.io>
2022-10-27 10:50:38 -0700

    We get all the way up to creating the sorted_nodes_mmap. Panics when doing copy_from_slice.

--- osmflatc/src/hilbert.rs
+++ osmflatc/src/hilbert.rs
@@ -1,14 +1,13 @@
+use core::mem::size_of;
+use core::slice::from_raw_parts_mut;
 use osmflat::*;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use log::info;
 use memmap2::MmapMut;
 use rayon::prelude::*;
 use std::io::{Error, ErrorKind};
-use std::{
-    fs::{File, OpenOptions},
-    path::PathBuf,
-};
+use std::{fs::OpenOptions, path::PathBuf};
 
 pub fn process(dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     let archive = Osm::open(FileResourceStorage::new(dir.clone()))?;
@@ -25,31 +24,44 @@ pub fn process(dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
         )));
     }
 
-    let nodes_len = archive.nodes().len();
-    let ways_len = archive.ways().len();
-    let relations_len = archive.relations().len();
-
-    let mut hilbert_node_pairs_buf = open_mmap(&dir, "hilbert_node_pairs")?;
-    let slc = &mut hilbert_node_pairs_buf[8..];
-    let hilbert_node_pairs = unsafe {
-        let pairs = ::core::slice::from_raw_parts_mut(
-            slc.as_ptr() as *mut HilbertNodePair,
-            hilbert_node_pairs_len,
-        );
-        pairs
-    };
-
-    // for idx in &hilbert_node_pairs[..30] {
-    //     println!("hilbert_node_pairs  i {} h {}", idx.i(), idx.h());
-    // }
+    let mut b = open_mmap(&dir, "hilbert_node_pairs")?;
+    let slc = &mut b[8..];
+    let hilbert_node_pairs =
+        unsafe { from_raw_parts_mut(slc.as_ptr() as *mut HilbertNodePair, hilbert_node_pairs_len) };
 
     info!("Sorting hilbert node pairs...");
     let t = Instant::now();
     hilbert_node_pairs.par_sort_unstable_by_key(|idx| idx.h());
-    info!(
-        "Finished sorting hilbert node pairs in {} secs.",
-        t.elapsed().as_secs()
-    );
+    info!("Finished in {} secs.", t.elapsed().as_secs());
+
+    // Get the original nodes vector to read from.
+    let nodes_len = archive.nodes().len();
+    let nodes_mmmap = open_mmap(&dir, "nodes")?;
+    let nodes = unsafe { from_raw_parts_mut(nodes_mmmap[8..].as_ptr() as *mut Node, nodes_len) };
+
+    // Setup new nodes vector to place sorted nodes into.
+    let mut sorted_nodes_mmap =
+        create_mmap(&dir, "sorted_nodes", 8 + size_of::<Node>() * nodes_len)?;
+    // Copy header data
+    sorted_nodes_mmap.copy_from_slice(&nodes_mmmap[..8]);
+    // let sorted_nodes =
+    //     unsafe { from_raw_parts_mut(sorted_nodes_mmap[8..].as_ptr() as *mut Node, nodes_len) };
+    let sorted_nodes_ptr = sorted_nodes_mmap[8..].as_ptr() as *mut Node;
+
+    // Parallel iteration through the original nodes vector.
+    info!("Copying nodes to sorted nodes vector.");
+    let t = Instant::now();
+    hilbert_node_pairs
+        .iter()
+        .enumerate()
+        .for_each(move |(i, pair)| {
+            let orig_node = &nodes[i as usize];
+            unsafe {
+                // sorted_nodes.get_unchecked_mut(pair.i() as usize).fill_from(orig_node);
+                (*sorted_nodes_ptr.add(pair.i() as usize)).fill_from(orig_node);
+            };
+        });
+    info!("Finished in {} secs.", t.elapsed().as_secs());
 
     Ok(())
 }
@@ -58,9 +70,19 @@ fn open_mmap(dir: &PathBuf, file_name: &str) -> std::io::Result<MmapMut> {
     let file = OpenOptions::new()
         .read(true)
         .write(true)
-        .create(true)
         .open(dir.join(file_name))?;
 
+    let mmap = unsafe { MmapMut::map_mut(&file)? };
+    Ok(mmap)
+}
+
+fn create_mmap(dir: &PathBuf, file_name: &str, size: usize) -> std::io::Result<MmapMut> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(dir.join(file_name))?;
+    file.set_len(size as u64);
     let mmap = unsafe { MmapMut::map_mut(&file)? };
     Ok(mmap)
 }
